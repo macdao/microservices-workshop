21454237f9f0408dfa172bd38d00d359923ded7d
Qi Xi <xqi@thoughtworks.com>
2017-06-21 11:09:24 +0800

    add eureka capability

--- consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -1,12 +1,38 @@
 package com.example.consumer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class ConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerApplication.class, args);
-	}
+    @LoadBalanced
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+
+    @RequestMapping("/")
+    public String home() {
+        final String response = restTemplate.getForObject("http://provider", String.class);
+        return "Consumer:" + response;
+    }
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
--- provider/src/main/java/com/example/provider/ProviderApplication.java
+++ provider/src/main/java/com/example/provider/ProviderApplication.java
@@ -2,11 +2,21 @@ package com.example.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class ProviderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProviderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Provider";
+    }
 }
