061adaad2fec2e7d7ed5ea2d655c1625590747cc
Qi Xi <xqi@thoughtworks.com>
2017-07-06 08:20:08 +0800

    add spring session

---
+++ gateway/src/main/java/com/example/gateway/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.savedrequest.NullRequestCache;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.session.web.http.HttpSessionStrategy;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().anyRequest().authenticated().and()
+                .requestCache().requestCache(new NullRequestCache()).and()
+                .httpBasic();
+    }
+
+    @Bean
+    public HttpSessionStrategy httpSessionStrategy() {
+        return new HeaderHttpSessionStrategy();
+    }
+}
